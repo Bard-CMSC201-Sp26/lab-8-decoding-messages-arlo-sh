6761814c9342ec9ad0dc7bead7e3b9016ff57946
ArloSH <as3901@bard.edu>
2026-04-24 09:44:30 -0400

    Update PrefixTree.java

--- PrefixTree.java
+++ PrefixTree.java
@@ -1,2 +1,16 @@
 class PrefixTree {
+	private Node root;
+
+	private class Node {
+		private Node left;
+		private Node right;
+		private Character c;
+		public Node(Character c)
+		{	this.c = c;		}
+	}
+	
+	public static void main(String[] args) {
+		PrefixTree tree = new PrefixTree();
+	}
+	
 }
