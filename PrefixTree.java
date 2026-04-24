af1f5ea2e701843f507e369e275b41469a3bef06
ArloSH <as3901@bard.edu>
2026-04-24 10:40:02 -0400

    Update PrefixTree.java
    update

--- PrefixTree.java
+++ PrefixTree.java
@@ -1,3 +1,8 @@
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
 class PrefixTree {
 	private Node root;
 
@@ -11,6 +16,18 @@ class PrefixTree {
 	
 	public static void main(String[] args) {
 		PrefixTree tree = new PrefixTree();
-	}
-	
+		File preOrder = new File("abra.txt");
+		String preOrderString = "";
+		String wordString = "";
+		try (Scanner Read = new Scanner(f)) {
+			while (hasNextLine()) {
+				String data = myReader.nextLine();
+				preOrderString += data;
+				System.out.println("preOrderString);
+			}
+			wordString += data.nextLine();
+		} catch (FileNotFoundException e) {
+			System.out.println("An error occurred.");
+		}
+    }
 }
